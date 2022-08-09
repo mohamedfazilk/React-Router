13e68d663e775fafb03d5c8b1692e4f7b2a8763c
mohamedfazilk <2114921@cognizant.com>
2022-08-09 14:51:59 +0530

    setup the error pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes> 
         <Route path='/' element={<Home/>}/> 
         <Route path='about' element={<About/>}/> 
-        <Route path='products' element={<Products/>}/>    
+        <Route path='products' element={<Products/>}/>  
+        <Route path='*' element={<Error/>}/>    
       
         
       </Routes>
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
         <section>
             <h2>Error</h2>
+            <p>Page not Found</p>
+            <Link to='/'>Back To Home</Link>
         </section>
 
     )
