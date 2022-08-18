e60c46964fa7ee7c488d744f9cbaa5337be4130f
mohamedfazilk <2114921@cognizant.com>
2022-08-18 11:30:49 +0530

    place user as a prop in login and setUser as a prop in dashboard

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
         <Route path='about' element={<About/>}/> 
         <Route path='products' element={<Products/>}/>  
         <Route path='products/:productId' element={<SingleProduct/>}/>
-        <Route path='login' element={<Login/>}/>
-        <Route path='dashboard' element={<Dashboard/>}/> 
+        <Route path='login' element={<Login setUser={setUser}/>}/>
+        <Route path='dashboard' element={<Dashboard user={user}/>}/> 
         <Route path='*' element={<Error/>}/>  
         </Route>  
       </Routes>
